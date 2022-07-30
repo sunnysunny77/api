8a452b5e7342c95b3749f08ea67123f7ef7f857d
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-07-30 21:33:53 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header ("Access-Control-Allow-Headers: *");
+header ("Access-Control-Allow-Headers: Authorization");
 
 $user = $_SERVER['PHP_AUTH_USER'];
 $pass = $_SERVER['PHP_AUTH_PW'];

249b57b9f9e2ca2b66c61609feecf3a606db0f12
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-10 17:23:33 +0800

    Update logout.class.php

--- classes/logout.class.php
+++ classes/logout.class.php
@@ -4,7 +4,11 @@ class Logout {
 
     public function __construct()
     {
-        
+
+        echo header('Access-Control-Allow-Methods: GET');
+        echo header("Access-Control-Allow-Headers: Cookie");
         setcookie("authorizationcookie", false, time() + (120*60), "/?controller=authorizationcookie", "localhost", true, true);
+        echo header("Connection: Close");
+        exit();
     }
 }

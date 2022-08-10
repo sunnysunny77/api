fd04f6d410952c67af2c570eb15cd6a2ee055cac
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-10 20:10:10 +0800

    Update registration.class.php

--- classes/registration.class.php
+++ classes/registration.class.php
@@ -21,11 +21,35 @@ class Registration
         echo header("Access-Control-Allow-Headers: Authorization");
         echo header('Access-Control-Allow-Methods: OPTIONS');
 
+        if (preg_match("/^[^0-9]+$/", $this->pass)) {
+
+            echo json_encode("Pass accepts one #");
+            exit();
+        }
+
+        if (preg_match("/^[^A-Z]+$/", $this->pass)) {
+
+            echo json_encode("Pass accepts one capital");
+            exit();
+        }
+
+        if (preg_match("/^[^a-z]+$/", $this->pass)) {
+
+            echo json_encode("Pass accepts one lowercase");
+            exit();
+        }
+
+        if (strlen($this->pass) > 19 || strlen($this->pass) < 8) {
+
+            echo json_encode("Pass accepts 8 to 19 characters");
+            exit();
+        }
+
         $pass = password_hash($this->pass, PASSWORD_DEFAULT);
 
         $this->model->SetUserPass($this->email, $pass);
 
-        setcookie("authorizationcookie", base64_encode(md5(true)), time() + (120*60), "/?controller=authorizationcookie", "localhost", true, true);
+        setcookie("authorizationcookie", base64_encode(md5(true)), time() + (120 * 60), "/?controller=authorizationcookie", "localhost", true, true);
         echo json_encode(base64_encode(md5(true)));
         echo header("Connection: Close");
         exit();
